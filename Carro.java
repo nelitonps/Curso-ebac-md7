1f7c4b68d34b54f6613e67d21e66c8b917cfb316
nelitonps <neliton.net@hotmail.com>
2023-06-15 17:03:16 -0300

    Tarefa modulo 7

---
+++ Carro.java
@@ -0,0 +1,41 @@
+/**
+ * @author Neliton
+ * @version 1.0
+ */
+
+public class Carro{
+
+    private String marca;
+    private String cor;
+    private double valor;
+
+    public Carro(){
+
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(int marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(int cor) {
+        this.cor = cor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+
+}
